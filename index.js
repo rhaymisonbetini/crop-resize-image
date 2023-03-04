0d2d49676cdd02dcc7345e6dc45bfd5900556936
RHAYMISON  CRISTIAN <rhaymison@hotmail.com>
2023-03-04 08:11:27 -0300

    resize-function

--- index.js
+++ index.js
@@ -6,9 +6,19 @@ class CropResizer {
         this.element = null;
     }
 
-    crop(imageTag = null, newX, newY, newWidth, newHeight,displayCanvas = false) {
+    /**
+     * this function crop a image by left top (newx, newY) coords and create a new imagine with a ginve width and height
+     * @param {string} imageTag 
+     * @param {number} newX 
+     * @param {number} newY 
+     * @param {number} newWidth 
+     * @param {number} newHeight 
+     * @param {number} displayCanvas 
+     * @returns {Promise}
+     */
+    crop(imageTag, newX, newY, newWidth, newHeight, displayCanvas = false) {
         return new Promise((resolve, reject) => {
-            if (!imageTag && !this.element) {
+            if (!imageTag && !newX && !newY && !newWidth && !newHeight && !this.element) {
                 reject({ error: 'ELEMENT_MUST_BE_PROVIDED' })
             }
             let uploatedImage = document.getElementById(imageTag)
@@ -33,12 +43,49 @@ class CropResizer {
         })
     }
 
+    /**
+     * this function resiize imagem with a give new width and height
+     * @param {string} imageTag 
+     * @param {number} newWidth 
+     * @param {number} newHeight 
+     * @param {number} displayCanvas 
+     * @returns {Promise}
+     */
+    resize(imageTag, newWidth, newHeight, displayCanvas = false) {
 
-    resize(imageTag = null) {
+        return new Promise((resolve, reject) => {
 
-        return this;
+            let uploatedImage = document.getElementById(imageTag)
+            let originalPath = uploatedImage.files[0]
+            const fileReader = new FileReader();
+            fileReader.readAsDataURL(originalPath);
+            fileReader.onload = async (event) => {
+                const originalImage = new Image();
+                originalImage.src = event.target.result;
+                const canvas = document.getElementById('canvas');
+                const ctx = canvas.getContext('2d');
+                !displayCanvas ? canvas.style = "display: none" : null;
+
+                originalImage.onload = async () => {
+                    const originalWidth = originalImage.naturalWidth;
+                    const originalHeight = originalImage.naturalHeight;
+                    const aspectRatio = originalWidth / originalHeight
+
+                    if (newHeight === undefined) {
+                        newHeight = newWidth / aspectRatio;
+                        newHeight = Math.floor(newHeight);
+                        hInput.placeholder = `Height (${newHeight})`;
+                        hInput.value = newHeight;
+                    }
+                    canvas.width = newWidth;
+                    canvas.height = newHeight;
+                    ctx.drawImage(originalImage, 0, 0, newWidth, newHeight);
+                    this.element = document.getElementById('canvas').toDataURL("image/jpeg", 0.9);
+                    resolve(this)
+                };
+            }
+        })
     }
-
 }
 
 module.exports = CropResizer;
